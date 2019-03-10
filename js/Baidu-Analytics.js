f061db1918cfe233403179ef9e9e0c9e8cf154da
Boss_Gan <gk1424301572@163.com>
2019-03-10 11:28:32 +0800

    添加网站统计

---
+++ js/Baidu-Analytics.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5b94756de87b260b647aed0a5ca70e7c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
