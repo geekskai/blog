4f68300082d2324e05b07523e2f8ca76b252d861
geekskai <geeks.kai@gmail.com>
2024-09-17 00:20:03 +0800

    fix: update sitemap routes to ensure consistent URL formatting

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,8 +12,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}/`,
+  const routes = ['', 'blog/', 'projects/', 'tags/'].map((route) => ({
+    url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
