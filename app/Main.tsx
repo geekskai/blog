f755fdf04cdfa6e397023c3df7d522bf735d8ab7
Gan Kai <geeks.kai@gmail.com>
2024-09-19 15:11:24 +0800

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -53,7 +53,7 @@ export default function Home({ posts }) {
                           {summary}
                         </div>
                       </div>
-                      <div className="text-base font-medium leading-6 text-right">
+                      <div className="text-right text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
