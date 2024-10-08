7ce1ff36052731397c511d4985f2258e14a64519
geekskai <geeks.kai@gmail.com>
2024-10-08 23:18:00 +0800

    Add Cloudflare blog setup guide; update newsletter API to edge runtime and modify tag data

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,6 +1,6 @@
 import { NewsletterAPI } from 'pliny/newsletter'
 import siteMetadata from '@/data/siteMetadata'
-
+export const runtime = 'edge'
 const handler = NewsletterAPI({
   // @ts-ignore
   provider: siteMetadata.newsletter.provider,
