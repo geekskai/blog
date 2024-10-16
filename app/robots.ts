1a575482fad503e9403b67655ec3199aa1ca1160
Gan Kai <geeks.kai@gmail.com>
2024-10-16 12:08:39 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/*/feed.xml'
     },
     sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
     host: siteMetadata.siteUrl,
