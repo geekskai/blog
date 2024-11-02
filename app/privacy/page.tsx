5c493a1cd8f87f8d5f78c990fc0d94ef92cce54d
geekskai <geeks.kai@gmail.com>
2024-11-02 22:59:57 +0800

    Update page metadata title from 'About' to 'Privacy'

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,11 +1,7 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 import Link from '@/components/Link'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({ title: 'Privacy' })
 
 const PrivacyPolicy = () => {
   return (
