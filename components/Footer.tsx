67cd593170f497d48613ae19614a3238d3f8bdf7
geekskai <geeks.kai@gmail.com>
2025-01-27 15:12:04 +0800

    Remove PayPal donation button from footer and adjust layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,21 +18,6 @@ export default function Footer() {
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
         </div>
 
-        <div className="mb-8 text-sm text-stone-500 dark:text-stone-400">
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_s-xclick" />
-            <input type="hidden" name="hosted_button_id" value="79YU2N5XY6CW2" />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input
-              type="image"
-              src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif"
-              name="submit"
-              title="PayPal - The safer, easier way to pay online!"
-              alt="Buy Now"
-            />
-          </form>
-        </div>
-
         <div className="mb-2 flex space-x-8 text-sm text-stone-500 dark:text-stone-400">
           <div className="flex items-center space-x-2">
             <div>{siteMetadata.author}</div>
@@ -43,6 +28,8 @@ export default function Footer() {
               {siteMetadata.title}
             </Link>
           </div>
+        </div>
+        <div className="mb-2 flex space-x-8 text-sm text-stone-500 dark:text-stone-400">
           <Link className=" hover:text-primary-600 dark:hover:text-primary-400" href="/privacy/">
             Privacy
           </Link>
