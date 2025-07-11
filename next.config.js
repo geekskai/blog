47399d804a095d64ade4db3a2b876365d4a48489
geekskai <geeks.kai@gmail.com>
2025-07-11 22:39:35 +0800

    fix: Update content for various blog articles to improve clarity and consistency, including quote formatting and tag data adjustments for enhanced SEO and user engagement

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app https://td.doubleclick.net;
+  frame-src giscus.app https://td.doubleclick.net https://www.youtube.com;
 `
 
 const securityHeaders = [
