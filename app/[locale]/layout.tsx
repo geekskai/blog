012ab983b28c9e34d28f0dce73e747d4ead05e81
geekskai <geeks.kai@gmail.com>
2025-11-30 22:56:13 +0800

    refactor: Add React import to layout component for improved functionality and maintainability.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import "css/tailwind.css"
 import "pliny/search/algolia.css"
 import "remark-github-blockquote-alert/alert.css"
+import React from "react"
 import { Analytics, AnalyticsConfig } from "pliny/analytics"
 import { SearchProvider, SearchConfig } from "pliny/search"
 import Header from "@/components/Header"
