7da9847b691b9d125892efe1c344e9c9c90a9697
geekskai <geeks.kai@gmail.com>
2025-12-02 19:28:29 +0800

    refactor: Remove unnecessary whitespace in layout.tsx for improved code cleanliness and readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,8 +110,6 @@ export const generateMetadata = async ({ params }: Props): Promise<Metadata> => 
   }
 }
 
-
-
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default async function RootLayout({ children, params }: Props) {
