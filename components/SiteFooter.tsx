24d64c41a4507a4c7ceb57c5dbb58e2d61a30477
Gan Kai <geeks.kai@gmail.com>
2026-01-12 10:06:27 +0800

    Update SiteFooter.tsx

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -8,7 +8,6 @@ import { FileText, Calculator, Zap, Heart, ExternalLink, ArrowLeftRight } from "
 import { useTranslations } from "next-intl"
 import LinkNext from 'next/link'
 
-
 // Popular tools data for footer
 const popularTools = [
   {
