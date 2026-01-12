f40196215cb2c474e5c8df35eed619c4c3dc2525
Gan Kai <geeks.kai@gmail.com>
2026-01-12 14:28:03 +0800

    Reorder header navigation links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
-  { href: "/", title: "header_nav_home" },
-  // { href: "/blog/", title: "header_nav_blog" },
+  // { href: "/", title: "header_nav_home" },
+  { href: "/blog/", title: "header_nav_blog" },
   // { href: "/tools/", title: "header_nav_tools" },
   // { href: "/tags/", title: "Tags" },
   // { href: "/projects/", title: "Projects" },
