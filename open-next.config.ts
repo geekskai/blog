a58640dc46b88369bdb925de7db8c376b6b46e11
geekskai <geeks.kai@gmail.com>
2026-01-21 21:26:07 +0800

    Remove OpenNext Cloudflare configuration files and dependencies to streamline project structure and eliminate unused code.

--- open-next.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare"
-
-export default defineCloudflareConfig({
-  // Use minimal caching to reduce bundle size
-  incrementalCache: "dummy",
-  tagCache: "dummy",
-})
