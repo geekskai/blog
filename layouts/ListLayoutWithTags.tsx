e0d3714e3ced7515e0c70be82d7a04bf62e9e23a
geekskai <geeks.kai@gmail.com>
2026-04-26 10:47:48 +0800

    chore: Update TypeScript route imports in next-env.d.ts and add React import in ListLayoutWithTags.tsx for improved compatibility and maintainability.

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -7,6 +7,7 @@ import { allAuthors, Authors, type Blog } from "contentlayer/generated"
 import Link from "@/components/Link"
 import Tag from "@/components/Tag"
 import siteMetadata from "@/data/siteMetadata"
+import React from "react"
 
 interface PaginationProps {
   totalPages: number
