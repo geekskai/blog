33bab40c1d1e417039d351c2e7c5bdef7abecf65
geekskai <geeks.kai@gmail.com>
2026-05-17 12:06:34 +0800

    chore: Remove Sell With Boost badge from SiteFooter to streamline design and reduce clutter

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -218,15 +218,6 @@ const SiteFooter = () => {
             </div>
           </div>
         </div>
-
-        <a href="https://sellwithboost.com" target="_blank" rel="noopener noreferrer nofollow">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://sellwithboost.com/badge/listing.svg"
-            alt="Listed on Sell With boost"
-            style={{ height: "40px", width: "auto" }}
-          />
-        </a>
       </div>
     </footer>
   )
