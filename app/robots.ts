f0793bda6604458a2ecd89005b3f98f9cc2a82d1
Gan Kai <geeks.kai@gmail.com>
2026-05-26 09:15:47 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,6 @@ export default function robots(): MetadataRoute.Robots {
   const commonDisallow = [
     "/*/feed.xml",
     "/api/",
-    "/_next/static/css/",
     "/admin/",
     ...localeDisallowPaths,
   ]
